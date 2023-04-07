20c547519f9c24f66099defa58c9f6df1aadaadd
StefanioHabrArticles <diggerss13@yandex.ru>
2023-04-07 22:38:10 +0300

    Endomorphism.cs

---
+++ GeneralizeThisAndThat/Algebra/Endomorphism.cs
@@ -0,0 +1,36 @@
+using System.Numerics;
+
+namespace GeneralizeThisAndThat.Algebra;
+
+public class Endomorphism<TGroup> : IRing<Endomorphism<TGroup>>
+    where TGroup :
+    IAdditionOperators<TGroup, TGroup, TGroup>,
+    IAdditiveIdentity<TGroup, TGroup>,
+    IUnaryNegationOperators<TGroup, TGroup>
+{
+    private readonly Func<TGroup, TGroup> _func;
+
+    private Endomorphism(Func<TGroup, TGroup> func) =>
+        _func = func;
+
+    public TGroup At(TGroup x) =>
+        _func(x);
+
+    public static implicit operator Endomorphism<TGroup>(Func<TGroup, TGroup> func) =>
+        new(func);
+
+    public static Endomorphism<TGroup> operator +(Endomorphism<TGroup> left, Endomorphism<TGroup> right) =>
+        (Func<TGroup, TGroup>)(x => left.At(x) + right.At(x));
+
+    public static Endomorphism<TGroup> AdditiveIdentity =>
+        (Func<TGroup, TGroup>)(_ => TGroup.AdditiveIdentity);
+
+    public static Endomorphism<TGroup> operator -(Endomorphism<TGroup> value) =>
+        (Func<TGroup, TGroup>)(x => -value.At(x));
+
+    public static Endomorphism<TGroup> operator *(Endomorphism<TGroup> left, Endomorphism<TGroup> right) =>
+        (Func<TGroup, TGroup>)(x => left.At(right.At(x)));
+
+    public static Endomorphism<TGroup> MultiplicativeIdentity =>
+        (Func<TGroup, TGroup>)(x => x);
+}
